caa0690940c783935cb633894003393fbb7d6204
SMK-BOTZZ <161198246+SMK-BOTZZ@users.noreply.github.com>
2024-03-09 18:44:18 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/Advance-File-Share-Bot /Advance-File-Share-Bot
+  git clone https://github.com/SMK-BOTZZ/MADARAZ /MADARAZ
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Advance-File-Share-Bot
+  git clone $UPSTREAM_REPO /MADARAZ
 fi
 cd /Advance-File-Share-Bot
 pip3 install -U -r requirements.txt
